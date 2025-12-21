494405b2a660becc912cff2ad32d81bd7607d537
Sanjayj-06 <sanjayjayakumar91@gmail.com>
2025-12-21 14:39:54 +0530

    Fix Vercel deployment configuration

--- aiconference/vite.config.js
+++ aiconference/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/aispectrum/', 
+  base: '/', 
   plugins: [react()],
   assetsInclude: ['**/*.JPG'], // Add this line to include JPG files with uppercase extension
 })

4a04ab4f889fcc70d636b6db675e90fb16cb7cd9
aiconsortium-psgct <aiconsortium@psgtech.ac.in>
2026-08-13 16:43:16 +0530

    Remove server port configuration from Vite config
    Removed server configuration for port 4731.

--- aiconference/vite.config.js
+++ aiconference/vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 4731
-  },
   base: '/aispectrum/', 
   plugins: [react()],
   assetsInclude: ['**/*.JPG', '**/*.JPEG', '**/*.PNG', '**/*.jpg', '**/*.jpeg', '**/*.png', '**/*.gif', '**/*.svg', '**/*.webp'],

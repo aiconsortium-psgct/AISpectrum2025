8eb85bfe744af88f445e1d9ecf9c7534c32c0b9e
aiconsortium-psgct <aiconsortium@psgtech.ac.in>
2026-08-13 16:34:38 +0530

    Update vite.config.js

--- aiconference/vite.config.js
+++ aiconference/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 4731
+  },
   base: '/', 
   plugins: [react()],
   assetsInclude: ['**/*.JPG', '**/*.JPEG', '**/*.PNG', '**/*.jpg', '**/*.jpeg', '**/*.png', '**/*.gif', '**/*.svg', '**/*.webp'],
